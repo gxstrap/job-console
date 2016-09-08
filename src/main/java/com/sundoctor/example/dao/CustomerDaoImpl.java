228c1342093d92d9aeada2e4a1f5e560dfefb4f3
Administrator <Administrator@Vincent-PC>
2016-09-08 13:10:36 +0800

    初始化提交

---
+++ src/main/java/com/sundoctor/example/dao/CustomerDaoImpl.java
@@ -0,0 +1,44 @@
+package com.sundoctor.example.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate4.HibernateCallback;
+import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
+import org.springframework.stereotype.Repository;
+
+import com.sundoctor.example.model.Customer;
+import com.sundoctor.example.service.SimpleService;
+
+@Repository("customerDao")
+public class CustomerDaoImpl extends HibernateDaoSupport {
+
+	private static final Logger logger = LoggerFactory.getLogger(SimpleService.class);
+
+	
+	@Autowired
+	public CustomerDaoImpl(SessionFactory sessionFactory){
+		super.setSessionFactory(sessionFactory);
+	}
+	
+	public Customer backupDateabase() {
+
+		return this.getHibernateTemplate().execute(new HibernateCallback<Customer>() {
+			public Customer doInHibernate(Session session) {
+				logger.info("Session2=={}", session);
+				Customer customer = (Customer) session.createQuery("from Customer where id = 1").uniqueResult();
+				logger.info("Customer2={}", customer);
+				return customer;
+			}
+		});
+	}
+
+	public void test() {
+		
+		Customer customer = this.getHibernateTemplate().get(Customer.class, 1);
+		logger.info("Customer={}", customer);
+	}
+
+}
---
+++ src/main/java/com/sundoctor/example/service/MyQuartzJobBean.java
@@ -0,0 +1,48 @@
+package com.sundoctor.example.service;
+
+import org.apache.commons.lang3.StringUtils;
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.PersistJobDataAfterExecution;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+@PersistJobDataAfterExecution
+@DisallowConcurrentExecution//不允许并发执行
+public class MyQuartzJobBean extends QuartzJobBean {
+
+	private static final Logger logger = LoggerFactory.getLogger(MyQuartzJobBean.class);
+
+	@Override
+	protected void executeInternal(JobExecutionContext jobexecutioncontext) throws JobExecutionException {
+		Trigger trigger = jobexecutioncontext.getTrigger();
+		String triggerName = trigger.getKey().getName();
+		String group = trigger.getKey().getGroup();
+
+		SimpleService simpleService = getApplicationContext(jobexecutioncontext).getBean("simpleService",
+				SimpleService.class);
+
+		// 根据Trigger组别调用不同的业务逻辑方法
+		if (StringUtils.equals(group, Scheduler.DEFAULT_GROUP)) {
+			simpleService.testMethod(triggerName, group);
+		} else {
+			simpleService.testMethod2(triggerName, group);
+		}
+	}
+
+	private ApplicationContext getApplicationContext(final JobExecutionContext jobexecutioncontext) {
+		try {
+			return (ApplicationContext) jobexecutioncontext.getScheduler().getContext().get("applicationContextKey");
+		} catch (SchedulerException e) {
+			logger.error("jobexecutioncontext.getScheduler().getContext() error!", e);
+			throw new RuntimeException(e);
+		}
+	}
+
+}
